1f43a259c1afd93430e9bccff35702d1e3e241b3
Ioan Filip <ioan.filip@evozon.com>
2022-07-15 10:12:11 +0300

    Added Request Validator tests

--- src/EvoEvents.API/Requests/Versions/GetVersionRequest.cs
+++ src/EvoEvents.API/Requests/Versions/GetVersionRequest.cs
@@ -1,7 +1,17 @@
-﻿namespace EvoEvents.API.Requests.Versions
+﻿using FluentValidation;
+
+namespace EvoEvents.API.Requests.Versions
 {
     public class GetVersionRequest
     {
         public string Name { get; set; }
+    }
+
+    public class GetVersionRequestValidator : AbstractValidator<GetVersionRequest>
+    {
+        public GetVersionRequestValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+        }
     }
 }
---
+++ src/EvoEvents.UnitTests/Api/Validators/Versions/GetVersionRequestValidatorTests.cs
@@ -0,0 +1,33 @@
+﻿using AutoFixture;
+using EvoEvents.API.Requests.Versions;
+using FluentAssertions;
+using NUnit.Framework;
+using System.Linq;
+
+namespace EvoEvents.UnitTests.Api.Validators.Versions
+{
+    [TestFixture]
+    public class GetVersionRequestValidatorTests
+    {
+        private GetVersionRequestValidator _validator;
+        private IFixture _fixture;
+
+        [SetUp]
+        public void Init()
+        {
+            _validator = new GetVersionRequestValidator();
+            _fixture = new Fixture();
+        }
+
+        [Test]
+        public void WhenNameMissing_ShouldReturnError()
+        {
+            var model = _fixture.Build<GetVersionRequest>()
+                .Without(x => x.Name)
+                .Create();
+            var result = _validator.Validate(model);
+
+            result.Errors.Any(error => error.PropertyName == "Name").Should().BeTrue();
+        }
+    }
+}
